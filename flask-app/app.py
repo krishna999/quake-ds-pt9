fd03aeb12e3c8ec1295eb28b127330d98828b6c9
Tally Wiesenberg <t.j.wies@gmail.com>
2020-04-27 23:32:32 -0400

    simple folium, not yet tested cause env issues

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,6 +1,8 @@
 from decouple import config
+import folium
 from flask import Flask, render_template, request
 
+from mapping.home_to_coords import city_to_coords
 
 def create_app():
     """Create and configure an instance of the Flask application"""
@@ -13,7 +15,8 @@ def create_app():
 
     @app.route('/map', methods=['POST', 'GET'])
     def map():
-
-        return render_template('map.html', title='Map data got!')
+        start_coords = city_to_coords('Los Angeles, CA')
+        folium_map = folium.Map(location=start_coords, zoom_start=10)
+        return folium_map._repr_html_()
 
     return app
