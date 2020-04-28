0ef75de26ed2daf3d92b497a66aa9a028f1d6da4
Tally Wiesenberg <t.j.wies@gmail.com>
2020-04-27 22:31:49 -0400

    renaming function

--- flask-app/mapping/home_to_coords.py
+++ flask-app/mapping/home_to_coords.py
@@ -2,7 +2,7 @@ import geocoder
 
 from decouple import config
 
-def find_coords(location):
+def city_to_coords(location):
     '''Return Tuple of longitude, lattitude (float, float) for given location (str) using Geocode.xyz API'''
 
     g = geocoder.mapquest(location, key=config('GEOCODE_AUTH'))
